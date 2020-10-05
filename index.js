acd95c5b3e3d6652c0e09ea317feb6e02cbd44df
kawsar00 <kawsarmahmud358@gmail.com>
2020-10-05 22:34:15 +0600

    updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const ObjectId = require('mongodb').ObjectId
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 
@@ -10,7 +11,6 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// console.log(process.env.DB_USER, process.env.DB_PASS, process.env.DB_NAME);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
@@ -18,8 +18,7 @@ client.connect(err => {
   const volunteerCollection = client.db("volunteerStore").collection("volunteerData");
   const registerDataCollection = client.db("registerDataStore").collection("registerData");
 
-  console.log('database connected');
-
+  //add all volunteer data
   app.post('/addData', (req, res) => {
     const volunteeringData = req.body
     console.log(volunteeringData);
@@ -30,7 +29,7 @@ client.connect(err => {
     })
   })
 
-  //read all products from server
+  //read all volunteer data from server
   app.get('/data', (req, res) => {
     volunteerCollection.find({})
     .toArray((err, documents) => {
@@ -38,8 +37,8 @@ client.connect(err => {
     })
   })
 
-  //add registerData form customer
-  app.post('/registerData', (req, res) => {
+  //add registerData form user
+  app.post('/addRegisterData', (req, res) => {
     const register = req.body 
     registerDataCollection.insertOne(register) 
     .then(result => {
@@ -47,25 +46,33 @@ client.connect(err => {
     })
   })
 
+  //read user registerData  
+  app.get('/registerData', (req, res) => {
+    registerDataCollection.find({})
+    .toArray((err, documents) => {
+      res.send(documents)
+    })
+  })
 
-  // app.post('/addUserData', (req, res) => {
-  //   const userData = req.body
-  //   console.log(userData)
-  //   volunteerCollection.insertOne(userData)
-  //   .then(result => {
-  //     res.send(result.insertedCount > 0)
-  //     console.log(result);
-  //   })
-  // })
+  //delete data form server
+  app.delete(`/delete/:id`, (req, res) => {
+    console.log(req.params.id)
+  
+    registerDataCollection.deleteOne({_id:ObjectId(req.params.id)})
+    .then(result =>{
+      // console.log(result)
+      res.send(result.deletedCount > 0)
+    })
+  
+  })
 
+  
 });
 
-
-
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello its working!')
 })
 
 
 const port = 5000
-app.listen(port)
+app.listen(process.env.PORT || port)
